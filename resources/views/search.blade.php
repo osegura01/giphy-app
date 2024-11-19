f22e59f54f9e46917fa4c996b1c3d035305a4b59
David Segura <osegura@redgps.com>
2024-11-19 09:55:15 -0600

    [UPGRADE] 2024-11-19 DSM - Se pone el ancho de la barra de búsqueda al 100% del div

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,17 +1,17 @@
 <form>
     <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="mt-2">
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8">
+                <div class="mt-10">
                     <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2
-                        focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                        focus-within:ring-inset focus-within:ring-indigo-600">
                         <input type="text" name="imgsearch" id="imgsearch" autocomplete="off"
                             class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900
                                 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                 placeholder="  search something" />
                     </div>
                 </div>
-                <div class="mt-2" id="imgresults"></div>
+                <div class="mt-10" id="imgresults"></div>
             </div>
         </div>
     </div>
