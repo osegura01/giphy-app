ef28f9bf0f8c8a419932ed88e9a3953a18e509a4
David Segura <osegura@redgps.com>
2024-11-19 09:56:44 -0600

    [UPGRADE] 2024-11-19 DSM - Se quita el padding y de 16 de la información del footer

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -1,4 +1,4 @@
-        <footer class="py-16 text-center text-sm text-black/70">
+        <footer class="text-center text-sm text-black/70">
             Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
         </footer>
     </body>
